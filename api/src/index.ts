ee9edeec56bd02d82d249bec2c0bc87a76f1c6ca
gusilebediflipflop228 <yurikikadet@gmail.com>
2026-06-20 13:19:20 +0600

    task 4.1

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,14 @@
-import express, {Request, Response} from 'express';
-import {randomUUID} from 'crypto';
+import express, { Request, Response } from 'express';
+import { randomUUID } from 'crypto';
+import { readFile, writeFile } from 'fs/promises';
+import path from 'path';
 
+const DB_PATH = path.join(__dirname, '..', 'data', 'sources.json'); // путь к файлу бд
 const app = express();
 app.use(express.json()); // парсинг JSON тела запроса(все запросы)
 
 const PORT = 4002;
-app.post('/api/sources', (req: Request, res: Response) => {
+app.post('/api/sources', async (req: Request, res: Response) => {
 
     const { name, secret, subscriberUrl } = req.body; // извлечение полей
 
@@ -13,15 +16,32 @@ app.post('/api/sources', (req: Request, res: Response) => {
         return res.status(400).json({ error: "Name is required" });
     }
 
-    const id = randomUUID();
-    console.log(`Создан источник: ${name} с ID: ${id}`); // для дебага логирование
+    try {
+        const fileContent = await readFile(DB_PATH, 'utf-8'); // чтение файла бд
+        const sources = fileContent ? JSON.parse(fileContent) : []; // если файл пустой, создаем пустой массив
 
-    res.status(201).json({
-        id,
-        name,
-        ingestUrl: `http://localhost:${PORT}/webhooks/${id}`,
-        hasSecret: !!secret // преобразование в бул
-    });
+        const newSource = {
+            id: randomUUID(),
+            name,
+            secret: secret || null,
+            subscriberUrl: subscriberUrl || null
+        };
+
+        sources.push(newSource);
+        await writeFile(DB_PATH, JSON.stringify(sources, null, 2));
+
+        console.log(`Создан источник: ${name} с ID: ${newSource.id}`); // для дебага логирование
+
+        res.status(201).json({
+            id: newSource.id,
+            name: newSource.name,
+            ingestUrl: `http://localhost:${PORT}/webhooks/${newSource.id}`,
+            hasSecret: !!newSource.secret // преобразование в бул
+        });
+    } catch (error) {
+        console.error("Ошибка при создании источника:", error);
+        res.status(500).json({ error: "Failed to save source" });
+    }
 });
 
 app.listen(PORT, () => {
